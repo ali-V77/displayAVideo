444402a8ff1c14834f147d1a79ef221e9d9146e6
vera-ali <351137340@qq,com>
2016-05-07 16:19:51 +0800

    display the local video

---
+++ displayVideo/displayVideo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  displayVideo
+//
+//  Created by 1 on 16/5/7.
+//  Copyright © 2016年 1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
